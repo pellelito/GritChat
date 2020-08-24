a0275b1e4825e6d210e846308886a209c8228ca3
pellelito <pelle.lofstrand@gmail.com>
2020-08-24 18:36:44 +0200

    Lektion1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+//Author: Pelle
+// A simple videochat app, made for a class in Javascript aug-aept 2020
+//Enjoy!!
+const peerOnOpen = (id) => {
+    document.querySelector('.my-peer-id').innerHTML= id;
+}
 
+const myPeerId = location.hash.slice(1);
+console.log(myPeerId);
+
+let peer = new Peer(myPeerId,{ host: "glajan.com", port:8443, path: "/myapp", secure: true});
+
+peer.on('open', peerOnOpen);
