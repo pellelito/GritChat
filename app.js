9ffe75035d0ce8f96b73ca38c63b7a8ea0423ddb
pellelito <pelle.lofstrand@gmail.com>
2020-08-26 13:11:05 +0200

    Upload

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-//Author: Pelle
-// A simple videochat app, made for a class in Javascript aug-aept 2020
-//Enjoy!!
+/***********************************************************************
+* Author: Pellelito                                                    *
+* A simple videochat app, made for a class in Javascript aug-sept 2020 *
+* Enjoy!!                                                              * 
+************************************************************************/
 
+// Sets user name on page
 const peerOnOpen = (id) => {document.querySelector('.my-peer-id').innerHTML= id};
-
+// Handles errors
 const peerOnError = (error) => {console.log(error)};
 
-let myPeerId = location.hash.slice(1);
-// console.log(myPeerId);
+// Get user
+let myPeerId;
+let getUser = () => {
+    let person = prompt("Please enter your name:", "Mr Potatohead");
+  if (person == null || person == "") {
+    myPeerId = "Mr Potatohead";
+  } else {
+    myPeerId = person;
+  }   
+}
+document.addEventListener('load', getUser()); 
 
 // Conect to peer server
 let peer = new Peer(myPeerId,{ host: "glajan.com", port:8443, path: "/myapp", secure: true});
@@ -16,32 +28,30 @@ let peer = new Peer(myPeerId,{ host: "glajan.com", port:8443, path: "/myapp", se
 peer.on('open', peerOnOpen);
 peer.on('error', peerOnError)
 
+
+//Refreshes online friends
 document.querySelector(".list-all-peers-button").addEventListener("click", () => {
-    //console.log("hej");
+  
     
     const peersEl = document.querySelector('.peers');
     peer.listAllPeers((peers) => {
-        //console.log(peers);
-        
+             
         const ul = document.createElement('ul');
-        
         peers.filter((peerId)=>{
             return peerId !== myPeerId ? true : false;
-            // .filter((p) => p !== myPeerId)
-            //if (peerId === myPeerId) return false;
-            //return true;
-        }).forEach(peerId => {
-            const li = document.createElement('li');
-            
-            const button = document.createElement('button');
-            button.innerText = peerId;
-            button.className = "connect-button";
-            button.classList.add('peerId-${peerId}');
-            li.appendChild(button);
-            ul.appendChild(li);
-            
-        });
+        })
         
+            .forEach(peerId => {
+            
+                const li = document.createElement('li');
+                const button = document.createElement('button');
+                button.innerText = peerId;
+                button.className = "connect-button";
+                button.classList.add('peerId-${peerId}');
+                li.appendChild(button);
+                ul.appendChild(li);
+            
+        }); 
         peersEl.appendChild(ul);
     });
 });
