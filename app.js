84f8fbb7e263af64007c907a4466b3c1c6983277
pellelito <pelle.lofstrand@gmail.com>
2020-08-26 08:19:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 //Author: Pelle
 // A simple videochat app, made for a class in Javascript aug-aept 2020
 //Enjoy!!
-const peerOnOpen = (id) => {
-    document.querySelector('.my-peer-id').innerHTML= id;
-}
 
-const myPeerId = location.hash.slice(1);
-console.log(myPeerId);
+const peerOnOpen = (id) => {document.querySelector('.my-peer-id').innerHTML= id};
 
+const peerOnError = (error) => {console.log(error)};
+
+let myPeerId = location.hash.slice(1);
+// console.log(myPeerId);
+
+// Conect to peer server
 let peer = new Peer(myPeerId,{ host: "glajan.com", port:8443, path: "/myapp", secure: true});
 
+// Handle Peer events
 peer.on('open', peerOnOpen);
+peer.on('error', peerOnError)
+
+document.querySelector(".list-all-peers-button").addEventListener("click", () => {
+    //console.log("hej");
+    
+    const peersEl = document.querySelector('.peers');
+    peer.listAllPeers((peers) => {
+        //console.log(peers);
+        
+        const ul = document.createElement('ul');
+        
+        peers.filter((peerId)=>{
+            return peerId !== myPeerId ? true : false;
+            // .filter((p) => p !== myPeerId)
+            //if (peerId === myPeerId) return false;
+            //return true;
+        }).forEach(peerId => {
+            const li = document.createElement('li');
+            
+            const button = document.createElement('button');
+            button.innerText = peerId;
+            button.className = "connect-button";
+            button.classList.add('peerId-${peerId}');
+            li.appendChild(button);
+            ul.appendChild(li);
+            
+        });
+        
+        peersEl.appendChild(ul);
+    });
+});
